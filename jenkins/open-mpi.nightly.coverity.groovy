1ddf86a1b8d29df6a2ddfe36cddcdb0fef3647bb
Brian W. Barrett <brian@bbarrett.org>
2022-11-04 20:07:03 -0700

    checkpoint

--- jenkins/open-mpi.nightly.coverity.groovy
+++ jenkins/open-mpi.nightly.coverity.groovy
@@ -16,14 +16,12 @@ pipeline {
 
     stages {
         stage('Hello') {
-            steps {
 	    	  sh "curl https://download.open-mpi.org/nightly/open-mpi/main/latest_snapshot.txt -O latest_snapshot.txt"
 		  version = sh(script: "cat latest_snapshot.txt", returnStdout: true).trim()
 		  tarball_name = "openmpi-${version}.tar.gz"
 		  sh "curl https://download.open-mpi.org/nightly/open-mpi/main/${tarball_name} -O ${tarball_name}"
 		  sh "tar -xf ${tarball_name}"
 		  sh ls -lR
-            }
         }
     }
 }
