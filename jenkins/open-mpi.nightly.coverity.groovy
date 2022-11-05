0e3c43558c8ab81a17236f16c44dfd4e902e72c3
Brian W. Barrett <brian@bbarrett.org>
2022-11-04 20:30:27 -0700

    checkpoint

--- jenkins/open-mpi.nightly.coverity.groovy
+++ jenkins/open-mpi.nightly.coverity.groovy
@@ -12,15 +12,15 @@ currentBuild.displayName = "#${currentBuild.number}"
 currentBuild.description = "description\n"
 
 node("ubuntu_20.04") {
-	stage('Hello') {
+	stage('Checkout') {
               def version = ""
 
 	      sh "curl https://download.open-mpi.org/nightly/open-mpi/main/latest_snapshot.txt -O"
   	      version = sh(script: "cat latest_snapshot.txt", returnStdout: true).trim()
   	      tarball_name = "openmpi-${version}.tar.gz"
   	      sh "curl https://download.open-mpi.org/nightly/open-mpi/main/${tarball_name} -O"
-  	      sh "mkdir scratch"
-	      sh "mkdir tools"
+  	      sh "mkdir -p scratch"
+	      sh "mkdir -p tools"
 	      sh "ls -lR"
         }
 }
