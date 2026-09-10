292d470d532ec8dfef4323434d9c22348048580a
Brian Barrett <bbarrett@amazon.com>
2026-09-10 13:24:32 -0700

    disable rpm test

--- jenkins/open-mpi.dist.create-tarball.groovy
+++ jenkins/open-mpi.dist.create-tarball.groovy
@@ -136,15 +136,15 @@ make distcheck VERBOSE=1"""
     }
   },
 
-  "rpm test suites" : {
-    node(rpm_builder) {
-      stage('RPM Build') {
-	prep_rpm_environment();
-	checkout_code();
-	sh "/bin/bash ${WORKSPACE}/ompi-scripts/jenkins/open-mpi.dist.create-tarball.build-rpm.sh ${build_prefix} ${srpm_name}"
-      }
-    }
-  },
+//  "rpm test suites" : {
+//    node(rpm_builder) {
+//      stage('RPM Build') {
+//	prep_rpm_environment();
+//	checkout_code();
+//	sh "/bin/bash ${WORKSPACE}/ompi-scripts/jenkins/open-mpi.dist.create-tarball.build-rpm.sh ${build_prefix} ${srpm_name}"
+//      }
+//    }
+//  },
 
   "tarball test suites" : {
     node(rpm_builder) {
